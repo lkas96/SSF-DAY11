ad768800c9ea71e10a7b3079bf0cfeebba9a255f
Lawson Kwek <63045323+lkas96@users.noreply.github.com>
2024-11-14 09:52:50 +0800

    Springboot Initializer Template

---
+++ src/main/java/sg/edu/nus/iss/vttp_day11_lecture/VttpDay11LectureApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp_day11_lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VttpDay11LectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VttpDay11LectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/vttp_day11_lecture/VttpDay11LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp_day11_lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VttpDay11LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
